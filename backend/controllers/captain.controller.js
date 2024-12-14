2c1411e4031d64514743b08572f5433cf0c58d56
Dammu Ravi Teja <dammuraviteja4@gmail.com>
2024-12-14 11:16:18 +0530

    added login route and middleware and working and tested

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -21,3 +21,22 @@ module.exports.registerCaptain = async (req, res, next) => {
     const token = captain.generateAuthToke();
     res.status(201).json({ token, captain });
 }
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    const captain = await captainModel.findOne({ email }).select('+password');
+    if (!captain) {
+        return res.status(404).json({ message: 'Captain not found' });
+    }
+    const isMatch = await captain.comparePassword(password);
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const token = captain.generateAuthToken();
+    res.cookie('token', token)
+    res.status(200).json({ token, captain });
+}
