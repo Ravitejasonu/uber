fd62fc69486fe17d8df014a20ded9bc16c9b60e4
Dammu Ravi Teja <dammuraviteja4@gmail.com>
2024-12-14 11:48:59 +0530

    created captain auth middleware logout route and controller profile route and controller

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 
 module.exports.authUser = async (req, res, next) => {
@@ -8,13 +10,34 @@ module.exports.authUser = async (req, res, next) => {
     if(!token) {
         return res.status(401).json({ message: 'Access denied' });
     }
-    const isblacklisted = await userModel.findOne({token: token});
+    const isblacklisted = await blackListTokenModel.findOne({token: token});
     if(isblacklisted){
         return res.status(401).json({ message: 'Access denied' });
     }
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await userModel.findById(decoded._id);
+        next();
+    }
+    catch(err) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    if(!token) {
+        return res.status(401).json({ message: 'Token not Found' });
+    }
+    const isblacklisted = await blackListTokenModel.findOne({token: token});
+    if(isblacklisted){
+        return res.status(401).json({ message: 'Access denied' });
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        req.captain = captain
+        
         next();
     }
     catch(err) {
